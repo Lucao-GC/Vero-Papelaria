53c9a2c9ae6e0ad6fa44a1fd15c2e1b40d048982
Lucao-GC <lucas.gcarvalho@sptech.school>
2025-08-27 16:40:07 -0300

    Barra de categorias

--- vero-papelaria/src/app/app.ts
+++ vero-papelaria/src/app/app.ts
@@ -2,11 +2,12 @@
 import { Component, signal } from '@angular/core';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { CategoriesBarComponent } from "./shared/categories-bar/categories-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet, CategoriesBarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
