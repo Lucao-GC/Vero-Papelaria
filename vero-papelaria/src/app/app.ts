5f372a63d13fd7178a0035826951c591d1675607
Lucao-GC <lucas.gcarvalho@sptech.school>
2025-08-28 16:43:22 -0300

    Secao de beneficios do site iniciada

--- vero-papelaria/src/app/app.ts
+++ vero-papelaria/src/app/app.ts
@@ -3,11 +3,12 @@ import { Component, signal } from '@angular/core';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 import { CategoriesBarComponent } from "./shared/categories-bar/categories-bar.component";
+import { BenefitsSectionComponent } from "./shared/benefits-section/benefits-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, RouterOutlet, CategoriesBarComponent],
+  imports: [NavbarComponent, RouterOutlet, CategoriesBarComponent, BenefitsSectionComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
