be6ee4c87b8ce0308cbc076dcbe1f4740ed9e2a4
Lucao-GC <lucas.gcarvalho@sptech.school>
2025-08-30 12:56:58 -0300

    Esqueleto do footer criado

--- vero-papelaria/src/app/app.ts
+++ vero-papelaria/src/app/app.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 import { CategoriesBarComponent } from "./shared/categories-bar/categories-bar.component";
 import { BenefitsSectionComponent } from "./shared/benefits-section/benefits-section.component";
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, RouterOutlet, CategoriesBarComponent, BenefitsSectionComponent],
+  imports: [NavbarComponent, RouterOutlet, CategoriesBarComponent, BenefitsSectionComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
